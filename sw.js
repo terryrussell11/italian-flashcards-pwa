88bc35b3504a7f8c66395771aea15696ae38589b
terryrussell11 <151549515+terryrussell11@users.noreply.github.com>
2026-02-28 10:25:36 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'it-flashcards-v17';
-const ASSETS = [
+const CACHE_NAME = 'it-flashcards-v21';
+const FILES = [
   './',
   './index.html',
   './style.css',
@@ -8,16 +8,23 @@ const ASSETS = [
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
+
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
